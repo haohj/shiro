d6bf2aae9ae17377a945d45fcf40a60b4a5fcb98
haohj <haohj@outlook.com>
2018-06-26 16:08:59 +0800

    jsp 修改

--- src/com/hao/shiro/realm/CustomRealm.java
+++ src/com/hao/shiro/realm/CustomRealm.java
@@ -1,4 +1,31 @@
 package com.hao.shiro.realm;
 
-public class CustomRealm {
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+public class CustomRealm extends AuthorizingRealm {
+    @Override
+    public String getName() {
+        return null;
+    }
+
+    @Override
+    public boolean supports(AuthenticationToken authenticationToken) {
+        return false;
+    }
+
+
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken token) throws AuthenticationException {
+        return null;
+    }
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection token) {
+        return null;
+    }
 }
