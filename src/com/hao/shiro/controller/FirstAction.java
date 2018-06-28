592a7fb60e686718f23cfc80a5cd4e794d9773f4
haohj <haohj@outlook.com>
2018-06-28 14:11:12 +0800

    整合shiro，自定义realm

--- src/com/hao/shiro/controller/FirstAction.java
+++ src/com/hao/shiro/controller/FirstAction.java
@@ -1,4 +1,8 @@
 package com.hao.shiro.controller;
+
+import com.hao.shiro.po.ActiveUser;
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.subject.Subject;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -6,15 +10,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class FirstAction {
-	//系统首页
-	@RequestMapping("/first")
-	public String first(Model model)throws Exception{
-		return "/first";
-	}
-	
-	//欢迎页面
-	@RequestMapping("/welcome")
-	public String welcome(Model model)throws Exception{
-		return "/welcome";
-	}
+    //系统首页
+    @RequestMapping("/first")
+    public String first(Model model) throws Exception {
+        //主体
+        Subject subject = SecurityUtils.getSubject();
+        //身份
+        ActiveUser activeUser = (ActiveUser) subject.getPrincipal();
+        model.addAttribute("activeUser", activeUser);
+        return "/first";
+    }
+
+    //欢迎页面
+    @RequestMapping("/welcome")
+    public String welcome(Model model) throws Exception {
+        return "/welcome";
+    }
 }	
--- src/com/hao/shiro/controller/LoginController.java
+++ src/com/hao/shiro/controller/LoginController.java
@@ -1,10 +1,14 @@
 package com.hao.shiro.controller;
 
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import com.hao.shiro.controller.exceptionResolver.CustomException;
 import com.hao.shiro.po.ActiveUser;
 import com.hao.shiro.service.SysService;
+import org.apache.shiro.authc.IncorrectCredentialsException;
+import org.apache.shiro.authc.UnknownAccountException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -28,22 +32,23 @@ public class LoginController {
 
 	//用户登陆提交
 	@RequestMapping("/login")
-	public String login(HttpSession session,String usercode,String password,String randomcode) throws Exception{
-
-		//校验验证码
-		//从session获取正确的验证码
-		String validateCode = (String)session.getAttribute("validateCode");
-		if(!randomcode.equals(validateCode)){
-			//抛出异常：验证码错误
-			throw new CustomException("验证码 错误 ！");
+	public String login( HttpServletRequest request) throws Exception{
+		// shiro在认证过程中出现错误后将异常类路径通过request返回
+		String exceptionClassName = (String) request
+				.getAttribute("shiroLoginFailure");
+		if(exceptionClassName!=null){
+			if (UnknownAccountException.class.getName().equals(exceptionClassName)) {
+				throw new CustomException("账号不存在");
+			} else if (IncorrectCredentialsException.class.getName().equals(
+					exceptionClassName)) {
+				throw new CustomException("用户名/密码错误");
+			} else if("randomCodeError".equals(exceptionClassName)){
+				throw new CustomException("验证码错误");
+			} else{
+				throw new Exception();//最终在异常处理器生成未知错误
+			}
 		}
-		//用户身份认证
-		ActiveUser activeUser = sysService.authenticat(usercode, password);
-		
-		//记录session
-		session.setAttribute("activeUser", activeUser);
-		
-		return "redirect:first.action";
+		return "login";
 	}
 	//退出
 	@RequestMapping("/logout")
