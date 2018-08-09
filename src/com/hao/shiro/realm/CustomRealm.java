8bc77d6092b05f5eb67645d01c031996df28d6ac
haohj <haohj94@163.com>
2018-08-09 14:43:16 +0800

    修改自定义realm

--- src/com/hao/shiro/realm/CustomRealm.java
+++ src/com/hao/shiro/realm/CustomRealm.java
@@ -12,7 +12,6 @@ import org.apache.shiro.subject.PrincipalCollection;
 import org.apache.shiro.util.ByteSource;
 import org.springframework.beans.factory.annotation.Autowired;
 
-import java.util.ArrayList;
 import java.util.List;
 
 public class CustomRealm extends AuthorizingRealm {
